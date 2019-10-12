20104d9793ace462fc52609860738dfe9fd17dce
shinsangeun <s0813se@naver.com>
2019-10-12 18:09:21 +0900

    로그인, 회원가입 폼 구현

--- dogs/views.py
+++ dogs/views.py
@@ -1,6 +1,33 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .forms import UserForm, LoginForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate
 
-from django.shortcuts import render
+def signup(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            new_user = User.objects.create_user(**form.cleaned_data)
+            login(request, new_user)
+            return redirect('index')
+    else:
+        form = UserForm()
+        return render(request, 'signup.html', {'form': form})
 
-# Create your views here.
+def signin(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username = username, password = password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            return HttpResponse('로그인 실패. 다시 시도 해보세요.')
+    else:
+        form = LoginForm()
+        return render(request, 'login.html', {'form': form})
