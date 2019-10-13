a4e8572e515ac94d0b7d20c335983ccf7d75f137
shinsangeun <s0813se@naver.com>
2019-10-13 22:52:33 +0900

    로그인 페이지 수정

--- walkdog/urls.py
+++ walkdog/urls.py
@@ -2,10 +2,12 @@ from django.conf.urls import url
 from django.contrib import admin
 from dogs import views
 from django.contrib.auth import views as auth_views
+from django.conf.urls import include
 
 app_name = 'dogs'
 
 urlpatterns = [
+    url(r'^/', include('django.contrib.auth.urls')),
     url(r'^accounts/signup/$', views.signup, name='signup'),
     url(r'^login/$', views.signin, name='login'),
 ]
